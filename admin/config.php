02c49db97aae6320760653e15b7e2f095b8c08d9
evgeniy selevko <evgeniyselevko@gmail.com>
2016-09-07 12:41:18 +0300

    begin project

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ihappy.loc/admin/');
+define('HTTP_CATALOG', 'http://ihappy.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ihappy.loc/admin/');
+define('HTTPS_CATALOG', 'http://ihappy.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/ihappy.loc/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/ihappy.loc/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/ihappy.loc/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/ihappy.loc/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/ihappy.loc/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/ihappy.loc/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/ihappy.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/ihappy.loc/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/ihappy.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/ihappy.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/ihappy.loc/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/ihappy.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ihappy');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ihappy.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ihappy.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/ihappy.loc/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/ihappy.loc/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/ihappy.loc/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/ihappy.loc/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/ihappy.loc/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/ihappy.loc/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/ihappy.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/ihappy.loc/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/ihappy.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/ihappy.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/ihappy.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ihappy');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
